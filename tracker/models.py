c6b6e0a4c29fadfa7961151fa82ac1bbc582de35
FahimAdib <fahim.adib01@gmail.com>
2021-03-05 12:43:42 +0600

    Added Most Functions and CSS

--- tracker/models.py
+++ tracker/models.py
@@ -4,15 +4,27 @@
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+import uuid
 
 
 class User(AbstractUser):
     pass
 
 class Transaction(models.Model):
+    uuid = models.CharField(default=uuid.uuid4, editable=True, unique=True, max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    category = models.CharField(max_length=200)
-    amount = models.DecimalField(max_digits=20)
-    
+    created  = models.DateTimeField(editable=True, default=timezone.now)
+    modified  = models.DateTimeField(editable=True, default=timezone.now)
+    category = models.CharField(max_length=200, default="")
+    debit = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    credit = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    description = models.CharField(max_length=200, default="")
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Transaction, self).save(*args, **kwargs)
+
--- tracker/views.py
+++ tracker/views.py
@@ -3,13 +3,37 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from django.db.models import Sum
+import datetime
 
-from .models import User
+from .models import User, Transaction
 
 # Create your views here.
 
 def index(request):
-    return render(request, "tracker/index.html")
+    if request.method == "POST":
+        tran = Transaction()
+        form = createTransaction(request.POST)
+        if form.is_valid():
+            tran.user = request.user
+            tran.category = form.cleaned_data['category']
+            tran.description = form.cleaned_data['descr']
+            if form.cleaned_data['value'] >= 0:
+                tran.credit = form.cleaned_data['value']
+            else:
+                tran.debit = form.cleaned_data['value']
+            tran.save()
+        
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "tracker/index.html", {
+            "form": createTransaction(),
+            "trans": Transaction.objects.all().filter(user=request.user, created__year=datetime.datetime.now().year, 
+                      created__month=datetime.datetime.now().month),
+            "credit": Transaction.objects.filter(user=request.user).aggregate(Sum('credit')),
+            "debit": Transaction.objects.filter(user=request.user).aggregate(Sum('debit')),
+        })
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +85,33 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "tracker/register.html")
+
+category_choices = (
+    ("Food", "Food"),
+    ("Income", "Income"),
+    ("Transport", "Transport"),
+    ("Bills", "Bills"),
+    ("Clothes", "Clothes")
+
+
+)
+
+
+class createTransaction(forms.Form):
+    category = forms.ChoiceField(choices=category_choices)
+    descr = forms.CharField(label="Description",
+                            widget=forms.TextInput())
+    value = forms.DecimalField(label="Value")
+
+
+def edit(request, uuid):
+    if request.method == "POST":
+        tran = Transaction()
+        form = createTransaction(request.POST)
+        if "delete" in request.POST:
+            Transaction.objects.all().filter(uuid=uuid).first().delete()
+    
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return HttpResponseRedirect(reverse("index"))
+
